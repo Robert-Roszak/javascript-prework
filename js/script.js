6886700278fe8f840340810e5b8f11dd0edde8c4
Robert Roszak <robert.roszak2@gmail.com>
2021-03-03 20:01:57 +0100

    Add functional game

--- js/script.js
+++ js/script.js
@@ -27,8 +27,33 @@ if(playerInput == '1'){
 else if (playerInput == '2'){
     playerMove ='papier';
 }
-else {
+else if (playerInput == '3') {
     playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam')
+}
+else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam');
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz');
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam');
+}
+else if (computerMove =='nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz');
+}
+else if (playerMove == 'nieznany ruch'){
+    printMessage('Oszukujesz? ;)');
+}
+else {
+    printMessage('Remis!')
+}
