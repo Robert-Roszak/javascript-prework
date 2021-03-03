e6adce1ef0f0e4c6ad314fb27e8f8a474127f625
Robert Roszak <robert.roszak2@gmail.com>
2021-03-03 16:15:29 +0100

    Add random number generator between 11 and 19

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ let computerMove = 'kamień';
 let playerMove ='papier';
 
 printMessage('Zagrałem ' + computerMove + '! Twój ruch to ' + playerMove + ' i przegrałem :(');
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+
+let randomNumber = Math.floor(math.random() * (19 - 11 + 1) + min);
+
+printMessage('Liczba przykładowa to: ' + randomNumber);
+printMessage('test message');
